5b9f10cf69e8bef0194625704683872d9f6a8406
Mitja Potocin <mitja.potocin@gmail.com>
2019-11-22 08:15:28 +0100

    Added image slider

---
+++ js/src/slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".slider").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
